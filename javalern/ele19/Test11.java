372cd46a509dcd9d327372809486ac068ed64841
zjgyjd <632032465@qq.com>
2018-11-22 23:53:08 +0800

    提交

--- javalern/ele19/Test11.java
+++ javalern/ele19/Test11.java
@@ -18,29 +18,34 @@ class CashierDesk{
 	}
 	
 	public void start(){
-		useIt();
+		useItForService();//服务操作
 	}
 	
-	public void useIt(){
-		Scanner s = new Scanner(System.in);
+	public void useItForService(){
+		Scanner sn = new Scanner(System.in);
 		while(true){
 			faceMune();
-			int chioce = s.nextInt();
+			int chioce = sn.nextInt();
 			if(chioce == 1){//开始使用
+				if(this.usedSize == 0){
+					System.out.println("没有货物上架");
+				}else{
+				shopList();
+				}
+			}else if(chioce == 2){//货物上架
 				int key ;
 				while(true){
 					System.out.println();
 					System.out.println();
-					System.out.println("1,2,3");
+					System.out.println("[1]添加商品,[2]移出商品,[3]返回,[4]修改商品");
 					shopList();
 					key = new Scanner(System.in).nextInt();
 					if(key == 3){
+						this.empty();
 						break;
 					}
 					listChioce(key);
 				}
-			}else if(chioce == 2){//设置
-				
 			}else if(chioce == 3){
 				System.out.println("谢谢使用!!");
 				break;
@@ -51,10 +56,19 @@ class CashierDesk{
 		}
 	}
 	
+	private void empty(){
+		while(usedSize != 0){
+			s[--usedSize] = null;
+		}
+	}
+	
 	private void listChioce(int key){
 		if(key == 1){
+			System.out.println("请输入商品id:");
 			int id = new Scanner(System.in).nextInt();
+			System.out.println("请输入商品姓名:");
 			String shop = new Scanner(System.in).nextLine();
+			System.out.println("请输入商品价格:");
 			int price = new Scanner(System.in).nextInt();
 			add(id,shop,price);
 		}else if(key == 4){
@@ -64,6 +78,20 @@ class CashierDesk{
 			chenge(id,property,d);
 		}else if(key == 2){
 			remove();
+		}else if(key == 5){
+			if(usedSize == 0){
+				System.out.println("没有商品需要结算!!");
+			}else{
+				int sum = 0; 
+				for(int i = 0;i < usedSize;i++){
+					sum += s[i].getprice();
+				}
+			System.out.println("开始结算.....");
+			System.out.println("总共为 "+ sum +"元");
+			this.empty();
+			}
+		}else{
+			System.out.println("输入有误");
 		}
 	} 
 	
@@ -141,6 +169,26 @@ class CashierDesk{
 		return null;
 	}
 	
+}
+
+class Waiter{
+	private Shop[] s;
+	private int canUse;
+	public Waiter(){
+		
+	}
+	public Waiter(Shop[] s, int c){
+		this.s = s;
+		this.canUse = c;
+	}
+	private void shopList(){
+		System.out.println("========================================");
+		System.out.println("编号             商品            价格($)");
+		for(int i = 0 ;i < this.canUse;i++){
+			System.out.println(s[i]);
+		}
+		System.out.println("========================================");
+	}
 }
 
 class Shop{
