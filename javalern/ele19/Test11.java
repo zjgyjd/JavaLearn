d1556a37ebd1c01dab1d886f1996e82dd1b53a10
zjgyjd <632032465@qq.com>
2018-11-23 23:33:22 +0800

    提交

--- javalern/ele19/Test11.java
+++ javalern/ele19/Test11.java
@@ -26,22 +26,24 @@ class CashierDesk{
 		while(true){
 			faceMune();
 			int chioce = sn.nextInt();
-			if(chioce == 1){//开始使用
+			if(chioce == 2){//开始使用
 				if(this.usedSize == 0){
-					System.out.println("没有货物上架");
+					System.out.println("请管理员先上架货物");
 				}else{
-				shopList();
+				waiterUse();
 				}
-			}else if(chioce == 2){//货物上架
+			}else if(chioce == 1){//货物上架
 				int key ;
 				while(true){
 					System.out.println();
 					System.out.println();
 					System.out.println("[1]添加商品,[2]移出商品,[3]返回,[4]修改商品");
 					shopList();
+					if(this.usedSize == 0){
+						System.out.println("目前还没有商品上架,请添加商品");
+					}
 					key = new Scanner(System.in).nextInt();
 					if(key == 3){
-						this.empty();
 						break;
 					}
 					listChioce(key);
@@ -56,6 +58,67 @@ class CashierDesk{
 		}
 	}
 	
+	
+	
+	private void waiterUse(){
+		Scanner sn = new Scanner(System.in);
+		while(true){
+			onlieList();
+			System.out.println();
+			System.out.println("===========================");
+			System.out.println("[1]进行下单,[2]查看,[3]返回");
+			System.out.println("===========================");
+			int chioce = sn.nextInt();
+			if(chioce == 1){
+				shopDown();
+			}else if(chioce == 2){
+				check();
+			}else if(chioce == 3){
+				break;
+			}else{
+			System.out.println("输入有误haha");
+			}
+		}
+	}
+	
+	private void check(){
+		Scanner sn = new Scanner(System.in);
+		System.out.println("请输入商品编号:");
+		int num = sn.nextInt();
+		if( num <= usedSize){
+			System.out.println("=================================");
+			System.out.println("商品编号: "+ num);
+			System.out.println(" 商品id :"+s[num - 1].getNum());
+			System.out.println("商品名称:"+s[num - 1].getShop());
+			System.out.println("商品价格:"+s[num - 1].getprice());
+			System.out.println("=================================");
+		}else{
+			System.out.println("请输入已有货物!!");
+		}
+	}
+	
+	private void shopList(){
+		System.out.println("==================================================");
+		System.out.println("商品id\t\t       商品\t\t      价格($)");
+		for(int i = 0 ;i < this.usedSize;i++){
+			System.out.println(s[i]);
+		}
+		System.out.println("==================================================");
+	}
+	
+	private void shopDown(){
+		Scanner sn = new Scanner(System.in);
+		System.out.println("请使用商品编号进行选择:");
+		int num = sn.nextInt();
+		if( num <= usedSize){
+			System.out.println("请输入数量:");
+			int n = sn.nextInt();
+			System.out.println("总价为" +n* s[num -1].getprice()+"元");
+		}else{
+			System.out.println("请输入已有货物!!");
+		} 
+	}
+	
 	private void empty(){
 		while(usedSize != 0){
 			s[--usedSize] = null;
@@ -63,19 +126,34 @@ class CashierDesk{
 	}
 	
 	private void listChioce(int key){
+		Scanner sn = new Scanner(System.in);
 		if(key == 1){
 			System.out.println("请输入商品id:");
 			int id = new Scanner(System.in).nextInt();
+			if(checkId(id)){
+				return;
+			}
 			System.out.println("请输入商品姓名:");
 			String shop = new Scanner(System.in).nextLine();
 			System.out.println("请输入商品价格:");
 			int price = new Scanner(System.in).nextInt();
 			add(id,shop,price);
 		}else if(key == 4){
-			int id = new Scanner(System.in).nextInt();
-			int property = 2;
-			String  d ="chenge"; 
-			chenge(id,property,d);
+			System.out.println("请输入商品id:");
+			int id = sn.nextInt();
+			for(int i = 0 ;i <usedSize;i++){
+				if(id == s[i].getNum()){
+					System.out.println("请输入想要修改的内容:");
+					System.out.println("[1]商品编号");
+					System.out.println("[2]商品名称");
+					System.out.println("[3]商品价格");
+					int property = sn.nextInt();
+					move(property,id);
+					break;
+				}else{
+					System.out.println("输入的编号不正确!!");
+				}
+			}
 		}else if(key == 2){
 			remove();
 		}else if(key == 5){
@@ -95,21 +173,57 @@ class CashierDesk{
 		}
 	} 
 	
+	private boolean checkId(int num){
+			for(int i = 0;i < usedSize;i++){
+				if(s[i].getNum()==num){
+					System.out.println("不予许输入重复id");
+					return true;
+				}
+			}
+			return false;
+		
+	}
+	
+	private void move( int property, int id){
+		Scanner sn = new Scanner(System.in);
+		if(property == 1){
+			System.out.println("开始修改商品编号....");
+			System.out.println("请输入要修改的编号:");
+			int num = sn.nextInt();
+			if(checkId(num)){
+				return;
+			}//id 不予许重复
+			chenge(id , property , num);
+		}else if(property == 2){
+			System.out.println("开始修改商品名称....");
+			System.out.println("请输入要修改的名称:");
+			String shopping = sn.nextLine();
+			chenge(id , property, shopping);
+		}else if(property == 3){
+			System.out.println("开始修改商品价格....");
+			System.out.println("请输入要修改的价格:");
+			int price = sn.nextInt();
+			chenge(id , property , price);
+		}else{
+			System.out.println("请输入正确选项");
+		}
+	}
+	
 	private void faceMune(){
 		System.out.println("========================================");
-		System.out.println("               1.开始使用");
-		System.out.println("	       2.设置");
+		System.out.println("               1.货物上架");
+		System.out.println("	       2.开始使用");
 		System.out.println("	       3.退出");
 		System.out.println("========================================");
 	}
 	
-	private void shopList(){
-		System.out.println("========================================");
-		System.out.println("编号             商品            价格($)");
+	private void onlieList(){
+		System.out.println("==================================================");
+		System.out.println("编号\t\t       商品\t\t      价格($)");
 		for(int i = 0 ;i < this.usedSize;i++){
-			System.out.println(s[i]);
+			System.out.println(" "+(i+1)+"\t\t\t"+s[i].getShop()+"\t\t\t"+s[i].getprice());
 		}
-		System.out.println("========================================");
+		System.out.println("==================================================");
 	}
 	
 	private void add(Integer n,String shop,Integer p){//增
@@ -122,7 +236,7 @@ class CashierDesk{
 		}else{
 			System.out.println("输入有误");
 		}
-	} 
+	}
 	
 	private void remove(){//删
 		if(this.usedSize==0){
@@ -171,25 +285,6 @@ class CashierDesk{
 	
 }
 
-class Waiter{
-	private Shop[] s;
-	private int canUse;
-	public Waiter(){
-		
-	}
-	public Waiter(Shop[] s, int c){
-		this.s = s;
-		this.canUse = c;
-	}
-	private void shopList(){
-		System.out.println("========================================");
-		System.out.println("编号             商品            价格($)");
-		for(int i = 0 ;i < this.canUse;i++){
-			System.out.println(s[i]);
-		}
-		System.out.println("========================================");
-	}
-}
 
 class Shop{
 	private int n ;
@@ -226,6 +321,6 @@ class Shop{
 		this.price = p;
 	}
 	public String toString(){
-		return "  "+this.n+"              "+this.shopping+"             "+this.price;
-	}
+		return " "+this.n+"\t\t\t"+this.shopping+"\t\t\t"+this.price;
+	} 
 }
