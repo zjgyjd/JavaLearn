b00dea006848ea376ccb5e99e52e4457520fe126
zjgyjd <632032465@qq.com>
2018-12-22 23:34:37 +0800

    提交

--- javalern/twe_20/Choose/PeopleChoose.java
+++ javalern/twe_20/Choose/PeopleChoose.java
@@ -5,15 +5,17 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class PeopleChoose {
+    static int end = 0;//控制程序退出
+    static int count = 0;
 
     /**
      * 读入女生文件
+     * 利用流输入读入数据
      *
      * @return String
      */
     public static String newFile(File file) {
         StringBuilder list = new StringBuilder();
-        //利用流输入读入数据
         try (FileInputStream in = new FileInputStream(file)) {
             Long size = file.length();
             byte[] buff = new byte[1024];
@@ -28,7 +30,7 @@ public class PeopleChoose {
     }
 
     /**
-     * 计算女生的满意度
+     * 计算男 女生的满意度
      *
      * @param man
      * @param girl
@@ -38,86 +40,213 @@ public class PeopleChoose {
         return man.getHopechar() * girl.getCharacter() + man.getHopelook() * girl.getLook() + (long) man.getHopemoney() * girl.getMoney();
     }
 
+    /**
+     * 输入所有女生的被满意度,能力值,id
+     *
+     * @param sum 满意度
+     * @param all 能力值
+     * @param id  id
+     * @return 返回最满意女生的下标
+     */
     public static int getNum(long[] sum, long[] all, long[] id) {
         long max = -1;
         int num = -1;
         for (int i = 0; i < sum.length; i++) {
             if (max < sum[i]) {
-                max = sum[i];
+                max = sum[i];//如果满意度有大的就覆盖max,并记录此时下标
                 num = i;
-            }
-            if (max == sum[i]) {
-                if (all[i] > all[num]) {
+            } else if (max == sum[i]) { //满意度相等时
+                if (all[i] > all[num]) {//能力大的先
                     max = sum[i];
                     num = i;
-                } else if (all[i] == all[num]) {
-                    if (id[i] < id[num]) {
+                } else if (all[i] == all[num]) {//能力如果相同,就比id
+                    if (id[i] < id[num]) { //id小优先
                         max = sum[i];
                         num = i;
                     }
                 }
             }
         }
-        return num;
+        return num;//返回下标
     }
 
+    /**
+     * 输入对象和一个链表,将此对象在链表中删除
+     * 可能存在问题的地方
+     *
+     * @param p
+     * @param people
+     */
+    public static void delet(People p, LinkedList<People> people) {
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).equals(p)) {
+                people.remove(i);
+                return;
+            }
+        }
+    }
+
+    /**
+     * 匹配规则函数:选出一队男女并提出
+     *
+     * @param copyMan  所剩男生
+     * @param copyGirl 所剩女生
+     */
+    public static void forChoose(LinkedList<People> copyMan, LinkedList<People> copyGirl) {
+        HashMap<People, ArrayList<People>> choose = new HashMap<>();//创建以女生为键,男生为链表的HashMap
+        long[] sum = new long[1001];//存储男生对女生满意度
+        long[] all = new long[1001];//存储所有女生能力值
+        long[] id = new long[1001];//存储该女生id
+
+        for (int i = 0; i < copyMan.size(); i++) {
+            for (int j = 0; j < copyGirl.size(); j++) {
+                sum[j] = clr(copyMan.get(i), copyGirl.get(j));//计算该男生对所有女生的满意度
+                all[j] = copyGirl.get(j).getall();//获取女生能力值和id
+                id[j] = copyGirl.get(j).getId();//获取id
+            }
+
+            int num = getNum(sum, all, id);//获得所有女生中满意度最高的列表下标
+            if (choose.get(copyGirl.get(num)) != null) {//判断当前这个键值是否存在
+                choose.get(copyGirl.get(num)).add(copyMan.get(i));//存在则直接将男生加入对象
+            } else { //该链表不存在,则直接创造新链表
+                ArrayList<People> forMan = new ArrayList<>();
+                forMan.add(copyMan.get(i));
+                choose.put(copyGirl.get(num), forMan);
+            }
+        }
+
+        Set<People> keySet = choose.keySet();//获取所有键值
+        People max = null;//表示最受欢迎的一个
+        int sizeMax = -1;//表示这个对象的下标
+        for (People s : keySet) { //找到选票最多的那个女生
+            if (choose.get(s).size() > sizeMax) {//先判断链表长度
+                max = s;
+                sizeMax = choose.get(s).size();
+            } else if (sizeMax == choose.get(s).size()) {//此时可能有两个女生链表长度相同
+                if (max == null) {
+                    System.out.println("wrong!!!!");
+                }
+                if (max.getall() < s.getall()) {//判断两者总值是谁大
+                    max = s;
+                    sizeMax = choose.get(s).size();
+                } else if (max.getall() == s.getall()) {//如果总值一样则比较id
+                    if (max.getId() > s.getId()) {//id小的先
+                        max = s;
+                        sizeMax = choose.get(s).size();
+                    }
+                }
+            }
+        }
+
+
+        ArrayList<People> temp = choose.get(max);//选出本轮女主角
+        while (temp.size() != 1) {//通过对长度的判断,选出本轮男主角
+            if (clr(max, temp.get(0)) > clr(max, temp.get(1))) {
+                temp.remove(1);//此时代表第一个大,移出第二个
+            } else if (clr(max, temp.get(0)) < clr(max, temp.get(1))) {
+                temp.remove(0);
+            } else { //此时满意度相等
+                if (temp.get(0).getall() < temp.get(1).getall()) {
+                    temp.remove(0);//比较能力
+                } else if (temp.get(0).getall() > temp.get(1).getall()) {
+                    temp.remove(1);
+                } else {//两个能力值相等
+                    if (temp.get(0).getId() < temp.get(1).getId()) {
+                        temp.remove(1);//比id
+                    } else {
+                        temp.remove(0);
+                    }
+                }
+            }
+        }
+
+        if (max.getId() == -1 || temp.get(0).getId() == -1) {//判断删除目标里面有没有主角Player
+            System.out.println(temp.get(0).getId() + ":" + max.getId());
+            end = 1;//将结束开关变为1,注意每个主角进场时都要重新变成0     !!!
+        }
+
+        delet(max, copyGirl);//此时HashMap里面已经一对一,之后将对应的元素在两个链表里面删除
+        delet(temp.get(0), copyMan);
+
+        choose = null;//重置所有
+    }
+
+    public static void start(Player p,LinkedList<People> listMan, LinkedList<People> listGirl){
+        if (p.sex == 1) {
+            //加入男性,放在第一个,注意保持始终100人
+            listMan.add(0, p.properties);
+        } else {
+            listGirl.add(0, p.properties);
+        }
+        //开始执行配对方法
+        while (end != 1) {
+            if (listGirl.size() == 0 || listMan.size() == 0) {
+                System.out.println("是个单身");
+                break;
+            }
+            forChoose(listMan, listGirl);
+        }
+    }
+
+    /**
+     * 主函数调用方法
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        long start  = System.currentTimeMillis();
         //创建双方对象储存位置
-        List<People> listMan = new LinkedList<>();
-        List<People> listGirl = new LinkedList<>();
-        HashMap<People, People> choose = new HashMap<>();
-        File maleFile = Paths.get("D:", "JavaLearn", "Java男女匹配作业(集合部分大作业)", "作业", "male.txt").toFile();
-        File femaleFile = Paths.get("D:", "JavaLearn", "Java男女匹配作业(集合部分大作业)", "作业", "female.txt").toFile();
+        LinkedList<People> listMan = new LinkedList<>();
+        LinkedList<People> listGirl = new LinkedList<>();
+        LinkedList<Player> lisrPlayer = new LinkedList<>();
+        File maleFile = Paths.get("D:","JavaLearn","Java男女匹配作业(集合部分大作业)","1000组待匹配数据", "male.txt").toFile();
+        File femaleFile = Paths.get("D:","JavaLearn","Java男女匹配作业(集合部分大作业)","1000组待匹配数据", "female.txt").toFile();
+        File playerFile = Paths.get("D:","JavaLearn","Java男女匹配作业(集合部分大作业)","1000组待匹配数据", "players.txt").toFile();
         String man = newFile(maleFile);
-        String[] mans = man.split("\n");
+
+        String[] mans = man.split(System.lineSeparator());
+
         for (String m : mans) {
             listMan.add(new People(m));
         }
         String girl = newFile(femaleFile);
-        String[] girls = girl.split("\n");
+        String[] girls = girl.split(System.lineSeparator());
         for (String g : girls) {
             listGirl.add(new People(g));
         }
-        //开始执行配对方法
-        long[] sum = new long[listMan.size()];
-        long[] all = new long[listMan.size()];
-        long[] id = new long[listMan.size()];
-        for (int i = 0; i < listMan.size(); i++) {
-            for (int j = 0; j < listGirl.size(); j++) {
-                //计算对所有女生的满意度
-                sum[j] = clr(listMan.get(i), listGirl.get(j));
-                //获取女生能力值和id
-                all[j] = listGirl.get(j).getall();
-                id[j] = listGirl.get(j).getall();
-            }
-            //获得所有女生中满意度最高的
-            int num = getNum(sum, all, id);
-            choose.put(listMan.get(i), listGirl.get(num));
+//
+//        LinkedList<People> copyMan = (LinkedList<People>) listMan.clone();
+//       LinkedList<People> copyWoman = (LinkedList<People>) listGirl.clone();
+//        String p = "0,9,56,31,7,73,20";
+//        Player s = new Player(p);
+//        start(s,copyMan,copyWoman);
+        //将主角加入配对
+        String play = newFile(playerFile);
+        String[] plays = play.split(System.lineSeparator());
+        for (String p : plays) {
+            lisrPlayer.add(new Player(p));
         }
-        int count = 0;
+       while(lisrPlayer.size()!=0){
+           LinkedList<People> copyMan = (LinkedList<People>) listMan.clone();
+           LinkedList<People> copyWoman = (LinkedList<People>) listGirl.clone();
+           Player p = lisrPlayer.remove();
+           start(p,copyMan,copyWoman);
+           end =0;
+       }
 
-        Set<People> keySet = choose.keySet();
-        for (People p : keySet) {
-            System.out.println(p.getId() + ":" + choose.get(p).getId());
-            count++;
-        }
-        System.out.println(count);
-        System.out.println(keySet.size());
-        System.out.println(listGirl.size());
-        System.out.println(listMan.size());
+        System.out.println(end-start);
     }
 }
 
-//1.创建一个代表男士的对象
+
 class People {
-    //2.属性代表该男士,和女士的数值
     private int id;//id
+    private int money;//财富
     private int look;//样貌
     private int character;//品质
-    private int money;//财富
+    private int hopemoney;//期望财富
     private int hopelook;//期望颜值
     private int hopechar;//期望品格
-    private int hopemoney;//期望财富
 
     @Override
     public boolean equals(Object o) {
@@ -168,9 +297,9 @@ class People {
 
     public People(int id, int look, int character, int money, int hopelook, int hopechar, int hopemoney) {
         this.id = id;
+        this.money = money;
         this.look = look;
         this.character = character;
-        this.money = money;
         this.hopelook = hopelook;
         this.hopechar = hopechar;
         this.hopemoney = hopemoney;
@@ -178,15 +307,28 @@ class People {
 
     //创造一个提取String的构造器
     public People(String all) {
-        String[] a = all.split(all);
+        String[] a = all.split(",");
         if (a.length == 7) {
             this.id = Integer.valueOf(a[0]);
-            this.look = Integer.valueOf(a[1]);
-            this.character = Integer.valueOf(a[2]);
-            this.money = Integer.valueOf(a[3]);
-            this.hopelook = Integer.valueOf(a[4]);
-            this.hopechar = Integer.valueOf(a[5]);
-            this.hopemoney = Integer.valueOf(a[6]);
+            this.money = Integer.valueOf(a[1]);
+            this.look = Integer.valueOf(a[2]);
+            this.character = Integer.valueOf(a[3]);
+            this.hopemoney = Integer.valueOf(a[4]);
+            this.hopelook = Integer.valueOf(a[5]);
+            this.hopechar = Integer.valueOf(a[6]);
+        }
+    }
+
+    public People(int id, String player) {
+        String[] a = player.split(",");
+        if (a.length == 7) {
+            this.id = id;
+            this.money = Integer.valueOf(a[1]);
+            this.look = Integer.valueOf(a[2]);
+            this.character = Integer.valueOf(a[3]);
+            this.hopemoney = Integer.valueOf(a[4]);
+            this.hopelook = Integer.valueOf(a[5]);
+            this.hopechar = Integer.valueOf(a[6]);
         }
     }
 
@@ -195,36 +337,17 @@ class People {
     }
 }
 
-class FeMale {
-    //2.属性代表该女士的数值
-    int id;//id
-    int look;//样貌
-    int character;//品质
-    int money;//财富
-    int hopelook;//期望颜值
-    int hopechar;//期望品格
-    int hopemoney;//期望财富
+class Player {
+    People properties;
+    int sex;
 
-    public FeMale(int id, int look, int character, int money, int hopelook, int hopechar, int hopemoney) {
-        this.id = id;
-        this.look = look;
-        this.character = character;
-        this.money = money;
-        this.hopelook = hopelook;
-        this.hopechar = hopechar;
-        this.hopemoney = hopemoney;
-    }
-
-    public FeMale(String all) {
-        String[] a = all.split(all);
-        if (a.length == 7) {
-            this.id = Integer.valueOf(a[0]);
-            this.look = Integer.valueOf(a[1]);
-            this.character = Integer.valueOf(a[2]);
-            this.money = Integer.valueOf(a[3]);
-            this.hopelook = Integer.valueOf(a[4]);
-            this.hopechar = Integer.valueOf(a[5]);
-            this.hopemoney = Integer.valueOf(a[6]);
+    public Player(String s) {//设置主角
+        String[] a = s.split(",");
+        if (Integer.valueOf(a[0]) == 0) {//Player没有id 0代表女性 1代表男性
+            properties = new People(-1, s); //设置主角id为-1
+        } else {
+            properties = new People(-1, s);
+            sex = 1;
         }
     }
 }
