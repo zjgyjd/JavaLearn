661186871395d5b0b86aa18a00a4f537cfed493e
zjgyjd <632032465@qq.com>
2019-03-07 23:15:43 +0800

    提交

---
+++ javalern/three_7/inbuilt/Person.java
@@ -0,0 +1,11 @@
+package com.github.zjgyjd.inbuilt;
+
+public class Person{
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
