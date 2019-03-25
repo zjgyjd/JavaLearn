d7222ac5b11708d7ed116aac445bd3773b000f94
zjgyjd <632032465@qq.com>
2019-03-25 23:28:17 +0800

    jdbc

--- jdbc/src/com/zjgyjd/jdbcTemplate.java
+++ jdbc/src/com/zjgyjd/jdbcTemplate.java
@@ -1,16 +1,14 @@
 package com.zjgyjd;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
 
 public abstract class jdbcTemplate {
     Connection connection = null;
-    PreparedStatement statement = null;
+    Statement statement = null;
     ResultSet resultSet = null;
 
-    public void loadDriverClass(){
+    public void loadDriverClass() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -18,34 +16,90 @@ public abstract class jdbcTemplate {
         }
     }
 
-    public void connection(){
-        //todo
+    public void connection(String url) {
+        try {
+            connection = DriverManager.getConnection(url, "root", "1234");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public abstract String praviderSql();
 
-    public  abstract <T> T  handlerResultSet();
+    public abstract <T> T handlerResultSet();
 
-    public void createStatement(){
-        //todo
+    public void createStatement() {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void closeResource(){
-        //todo
+    public void closeResource() {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public final <T> T execute(){
+    public final <T> T execute() {
         this.loadDriverClass();
-        this.connection();
+        String url = this.getString("memo");
+        this.connection(url);
         this.createStatement();
         String sql = this.praviderSql();
-        if(sql.trim().startsWith("select")||sql.trim().startsWith("SELECT")){
-            //todo
-        }else {
-            //todo
+        if (sql.trim().startsWith("select") || sql.trim().startsWith("SELECT")) {
+            try {
+                resultSet = statement.executeQuery(sql);
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    LocalDateTime createTime = resultSet.getTimestamp("created_time")
+                            .toLocalDateTime();
+
+                    LocalDateTime modifyTime = resultSet.getTimestamp("modify_time")
+                            .toLocalDateTime();
+
+                    System.out.println(id + "  " + name + "  " + createTime + "  " + modifyTime);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            int rSet  = 0;
+            try {
+                rSet = statement.executeUpdate(sql);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println(rSet);
         }
         //close用finally
         return null;
+    }
+
+    private String getString(String dataBase) {
+
+        return "jdbc:mysql://localhost:3306/" + dataBase;
     }
 }
 
