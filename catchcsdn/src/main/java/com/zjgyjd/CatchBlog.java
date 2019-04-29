ea71cbc9d0929f3187e5942e1067faef15ac5ee3
zjgyjd <632032465@qq.com>
2019-04-30 00:22:55 +0800

    commit

--- catchcsdn/src/main/java/com/zjgyjd/CatchBlog.java
+++ catchcsdn/src/main/java/com/zjgyjd/CatchBlog.java
@@ -1,8 +1,23 @@
 package com.zjgyjd;
 
-public class CatchBlog implements PageProcessor{
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class CatchBlog implements PageProcessor {
     private String userName = "zjgyjd";
     private int size = 0; //文章数
-    private Site
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
 
 }
