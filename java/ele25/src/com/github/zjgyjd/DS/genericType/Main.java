891d2a7a39becf018781862eb5c1974c9fb900ab
zjgyjd <632032465@qq.com>
2019-05-09 23:41:17 +0800

    commit

---
+++ java/ele25/src/com/github/zjgyjd/DS/genericType/Main.java
@@ -0,0 +1,36 @@
+package com.github.zjgyjd.DS.genericType;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    private static void testList(List<Integer> list){
+        list.clear();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        //[0,9]
+        Iterator<Integer> iterator = list.iterator();
+        while(iterator.hasNext()){
+            int item = iterator.next();//自动拆箱
+            System.out.println(item);
+        }
+
+        iterator = list.iterator();
+        while(iterator.hasNext()){
+            int item = iterator.next();
+            if(item == 5){
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
+    }
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> seqList = new SeqList<>();
+        testList(seqList);
+        testList(list);
+    }
+}
