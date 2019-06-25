48a9e3031a9b8c7b80e33f740e04f4053aa900b2
zjgyjd <632032465@qq.com>
2019-06-26 01:26:05 +0800

    commit

--- WebProjects/mybatis/src/main/java/com/zjgyjd/po/Items.java
+++ WebProjects/mybatis/src/main/java/com/zjgyjd/po/Items.java
@@ -2,4 +2,8 @@ package com.zjgyjd.po;
 
 public class Items {
 
+    public static void main(String[] args) {
+
+
+    }
 }
---
+++ WebProjects/mybits_reverstar/src/main/java/com/zjgyjd/mapper/OrderDetailMapper.java
@@ -0,0 +1,17 @@
+package com.zjgyjd.mapper;
+
+import com.zjgyjd.po.OrderDetail;
+
+public interface OrderDetailMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderDetail record);
+
+    int insertSelective(OrderDetail record);
+
+    OrderDetail selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderDetail record);
+
+    int updateByPrimaryKey(OrderDetail record);
+}
