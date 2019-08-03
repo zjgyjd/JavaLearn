148346cb4b2b429784e757b298a2de5983098ddc
zjgyjd <632032465@qq.com>
2019-08-03 23:39:09 +0800

    commit

--- Redislike/src/main/java/LRANGECommand.java
+++ Redislike/src/main/java/LRANGECommand.java
@@ -10,6 +10,20 @@ public class LRANGECommand implements Command {
     @Override
     public void run() {
         DateBase dateBase = DateBase.getInstance();
-
+        String key = new String((byte[]) args.remove(0));
+        List<String> list = dateBase.getList(key);
+        int start = Integer.valueOf(new String((byte[]) args.remove(0)));
+        int end = Integer.valueOf(new String((byte[]) args.remove(0)));
+        if (start < 0) {
+            start = 0;
+            if (end < 0) {
+                end = 0;
+            }
+        } else if (end >= list.size()) {
+            end = list.size() - 1;
+        }
+        for (int i = start; i <= end; i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
--- Redislike/src/main/java/Main.java
+++ Redislike/src/main/java/Main.java
@@ -6,11 +6,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, RedisException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String message = "*5\r\n$5\r\nLPUSH\r\n$3\r\nkey\r\n$1\r\n1\r\n$1\r\n2\r\n$1\r\n3\r\n";
-        ByteInputStream is = new ByteInputStream(message.getBytes(), message.getBytes().length);
+        String message1 = "*5\r\n$5\r\nLPUSH\r\n$3\r\nkey\r\n$1\r\n1\r\n$1\r\n2\r\n$1\r\n3\r\n";
+        String message2 = "*4\r\n$6\r\nLRANGE\r\n$3\r\nkey\r\n$1\r\n2\r\n$1\r\n8\r\n";
+        ByteInputStream is = new ByteInputStream(message1.getBytes(), message1.getBytes().length);
+        ByteInputStream is2 = new ByteInputStream(message2.getBytes(), message2.getBytes().length);
 
         Command list = Protocol.readCommand(new ProtocolInputStream(is));
+        Command list2 = Protocol.readCommand(new ProtocolInputStream(is2));
         list.run();
+        list2.run();
 //        InputStream is = System.in;
 
 
