f2e0c19650ab2f9e7adc9640190fe5fbba08d887
zjgyjd <632032465@qq.com>
2020-01-23 22:52:54 +0800

    commit

--- springmvc_fileupload/src/main/java/controller/UserController.java
+++ springmvc_fileupload/src/main/java/controller/UserController.java
@@ -1,11 +1,14 @@
 package controller;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -53,4 +56,47 @@ public class UserController {
         }
         return "success";
     }
+
+    @RequestMapping("/fileUpload_2")
+    public String fileUpload_2(HttpServletRequest request, MultipartFile upload) throws Exception {
+        System.out.println("springMVC文件上传....");
+        //使用fileupload组件完成文件上传
+        //上传位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        //判断该路径是否存在
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String filename = upload.getOriginalFilename();
+        //把文件名称设置为唯一值
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename = uuid + "_" + filename;
+        //完成文件上传
+        upload.transferTo(new File(path, filename));
+        return "success";
+
+    }
+
+    @RequestMapping("/fileUpload_3")
+    public String fileUpload_3(HttpServletRequest request, MultipartFile upload) throws Exception {
+        System.out.println("springMVC跨服务器文件上传....");
+        //定义上传文件路径,注意要加最后的斜杠
+        String path = "http://localhost:9090/uploads/";
+
+        String filename = upload.getOriginalFilename();
+        //把文件名称设置为唯一值
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename = uuid + "_" + filename;
+        //完成文件上传,跨服务器上传
+        //创建客户端对象
+        Client client = Client.create();
+        //和图片服务器进行连接
+        WebResource webResource = client.resource(path + filename);
+        //上传文件
+        webResource.put(upload.getBytes());
+        return "success";
+
+    }
 }
+
--- springmvc_fileupload/src/main/webapp/index.jsp
+++ springmvc_fileupload/src/main/webapp/index.jsp
@@ -11,8 +11,18 @@
     <title>文件上传</title>
 </head>
 <body>
-<h3>文件上传</h3><br>
+<h3>传统文件上传</h3><br>
 <form action="/user/fileUpload_1" method="post" enctype="multipart/form-data">
+    选择文件:<input type="file" name="upload" /><br>
+    <input type="submit" value="上传"/>
+</form>
+<h3>SpringMVC文件上传</h3><br>
+<form action="/user/fileUpload_2" method="post" enctype="multipart/form-data">
+    选择文件:<input type="file" name="upload" /><br>
+    <input type="submit" value="上传"/>
+</form>
+<h3>SpringMVC跨服务器文件上传</h3><br>
+<form action="/user/fileUpload_3" method="post" enctype="multipart/form-data">
     选择文件:<input type="file" name="upload" /><br>
     <input type="submit" value="上传"/>
 </form>
